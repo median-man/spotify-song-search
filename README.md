064423ffe70906c39920bd6a06512f0f10d88c08
John Desrosiers <john.desro@gmail.com>
2020-10-22 07:21:54 -0700

    Initial home page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spotify Search Example
+
+Example usage of the Spotify API to enable users to search for song info.
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static("public", { extensions: ["html"] }));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
