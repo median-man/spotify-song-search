4009dba97c0b9c0671708a3f47e5a57be30a24ee
John Desrosiers <john.desro@gmail.com>
2020-10-22 08:39:40 -0700

    Add spotify controller

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const spotifyApiController = require("./controllers/spotify-api");
+
 const port = process.env.PORT || 3000;
 app.use(express.static("public", { extensions: ["html"] }));
+
+app.get("/api/search/songs", spotifyApiController.searchTracks);
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
